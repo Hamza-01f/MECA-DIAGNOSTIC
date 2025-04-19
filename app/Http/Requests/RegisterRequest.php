41ade1264a43616a64d3e5783a60f13b94fc4f91
hamza <hamzaprograme@gmail.com>
2025-04-19 22:22:06 +0000

    adding the request form for login and register page

---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'Le nom est requis',
+            'email.required' => 'L\'email est requis',
+            'email.email' => 'Veuillez entrer une adresse email valide',
+            'email.unique' => 'Cet email est déjà utilisé',
+            'password.required' => 'Le mot de passe est requis',
+            'password.min' => 'Le mot de passe doit contenir au moins 6 caractères'
+        ];
+    }
+}
